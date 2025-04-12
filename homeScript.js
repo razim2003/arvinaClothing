4fa9141e03685f0bb2b879f7375ad5fa3fd514c3
Mohamed Sharfan <sharfansaleem72@gmail.com>
2025-04-12 17:08:01 +0530

    loginpage js updated

--- homeScript.js
+++ homeScript.js
@@ -1,11 +1,31 @@
-let userName = document.getElementById("userName").value;
-let password = document.getElementById("passWord").value;
+function login() {
+    let userName = document.getElementById("userName").value;
+    let password = document.getElementById("passWord").value;
 
 
-if(userName == 'admin' && password =='admin'){
+        if(userName == 'admin' && password =='admin'){
 
+            let fullWeb = document.getElementById("full");
+            let navv = document.getElementById("navi");
+            let loginpage = document.querySelector(".backg");
+
+            fullWeb.style.display = "block";
+            navv.style.display = "block";
+            loginpage.style.display= "none";
+        }
+        else{
+            document.getElementById("userName").value = ''
+            document.getElementById("passWord").value = ''
+            alert("Incorrect username or password!!");
+            let fullWeb = document.getElementById("full");
+            let navv = document.getElementById("navi");
+            let loginpage = document.querySelector(".backg");
+            fullWeb.style.display = "none";
+            navv.style.display = "none";
+            loginpage.style.display= "block";
+        
+        }
 }
-
 function displayLogin(){
     let fullWeb = document.getElementById("full");
     let navv = document.getElementById("navi");
@@ -13,16 +33,5 @@ function displayLogin(){
     fullWeb.style.display = "none";
     navv.style.display = "none";
     loginpage.style.display= "block";
-    console.log(userName);
-}
-
-function displayHome(){
-    let fullWeb = document.getElementById("full");
-    let navv = document.getElementById("navi");
-    let loginpage = document.querySelector(".backg");
-    fullWeb.style.display = "block";
-    navv.style.display = "block";
-    loginpage.style.display= "none";
-
 }
 
